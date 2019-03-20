dd55f15f4027d56c7dcc56faf762faf3a74f1e73
Tom Corke <tom.corke@skyscanner.net>
2019-03-20 11:51:57 +0000

    Updates

---
+++ precache-manifest.1501affc63ca963bbf6605b02f9b1d94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c07289564c7f1477155f",
+    "url": "/static/js/main.b1e7a4dd.chunk.js"
+  },
+  {
+    "revision": "1f2ecddc3bf1a8cf18d1",
+    "url": "/static/js/2.68a1ccac.chunk.js"
+  },
+  {
+    "revision": "c07289564c7f1477155f",
+    "url": "/static/css/main.ae8aa36c.chunk.css"
+  },
+  {
+    "revision": "a308d71bde2d8f1d61286d3df296c0e0",
+    "url": "/index.html"
+  }
+];
